c0e013979a0546d1212369e8d71fb1442ee3b235
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-18 23:04:55 +0000

    added function to generate random string and form to submit post request

--- express_server.js
+++ express_server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+const generateRandomString = function(){
+  const result = [];
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+  const numChars = 6 + 1;
+  for (let i = 0; i < numChars; i++){
+    let c = Math.floor(Math.random() * 61);
+    console.log(c);
+    result.push(chars[c]);
+  }
+  return result.join('');
+}
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello~!");
@@ -26,9 +44,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) =>{
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
 });
 
 
