ab946754abfaf9dd93d0ec4e64ea77a4aae09e74
Evelyn Yoa <yoaevelyn@gmail.com>
2021-01-21 02:17:49 +0000

    new registerd users will have random generated id and cleanup code

--- express_server.js
+++ express_server.js
@@ -14,8 +14,7 @@ app.use(cookieParser());
 
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "user2RandomID" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "spongebob" },
-  PgM3djJ:{ longURL: "https://www.example.edu", userID: "spongebob2" }
+  i3BoGr: { longURL: "https://www.google.ca", userID: "spongebob" }
 };
 
 const users = {
@@ -28,16 +27,13 @@ const users = {
     id: "user2RandomID",
     email: "snacks@latenight.com",
     password: "cookies"
-  },
-  "spongebob2": {
-    id: "spongebob2",
-    email: "pineapple@undertheC.com",
-    password: "123"
-  },
+  }
 
 };
 
+// Section of helper functions
 
+// generates string for tiny URLS and maybe user ids
 const generateRandomString = function() {
   const result = [];
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
@@ -49,10 +45,9 @@ const generateRandomString = function() {
   return result.join('');
 };
 
-
-const nonValidReg = function(newMail, pass) {
+// checks if the the email already exists
+const notValidReg = function(newMail, pass) {
   if (!newMail || !pass) {
-    console.log("empty");
     return true;
   }
   for (let user in users) {
@@ -63,21 +58,23 @@ const nonValidReg = function(newMail, pass) {
   return false;
 };
 
-const isUser = function(mail, pass) {
+// checks for user authentication
+const getUser = function(mail, pass) {
   if (!mail || !pass) {
-    console.log("empty login details");
     return false;
   }
-  if (users[mail]) {
-    if (users[mail].password === pass) {
-      console.log('correct login');
-      return true;
+  for (const user in users) {
+    if (users[user].email === mail) {
+      if (users[user].password === pass) {
+        return users[user].id;
+      }
     }
   }
   console.log("not valid user");
   return false;
 };
 
+// returns URL objects that belong to the user id
 const urlsForUser = function(id) {
   const results = {};
   for (const entry in urlDatabase) {
@@ -88,51 +85,46 @@ const urlsForUser = function(id) {
   return results;
 };
 
+// checks if the a URL object belongs to the current user
 const isOwnURL = function(cookieID, urlID) {
-  console.log("Is checking is OWN URL ==================================");
-  console.log(`cookieID is ${cookieID}`);
-  console.log(`URLID is ${urlID}`);
-  // console.log(`cookieID is ${cookieID} getting db user info ${urlDatabase[URLID].userID}`);
   if (!cookieID || !urlID) {
-    console.log("empty entries");
     return false;
   } else if (urlDatabase[urlID].userID === cookieID) {
-    console.log("matches");
     return true;
   }
 };
 
 
-
+// route for Home page
 app.get("/", (req, res) => {
   res.send("Hello~! Welcome ");
 });
 
+// route for list of urls (belonging to specific user)
 app.get("/urls", (req, res) => {
   let msg = '';
   //If not logged in display message to log in or register
   if (!req.cookies["user_id"]) {
     msg = "Please login";
-    console.log("Needs to login");
-    // res.send('Please login or Register');
-    // res.redirect('/urls/login');
   }
   let test = urlsForUser(req.cookies["user_id"]);
   const templateVars = {urls: test, "user": users[req.cookies["user_id"]], msg: msg};
   res.render("urls_index", templateVars);
 });
 
+
 // route to page that can create new tiny URLS
 app.get("/urls/new", (req, res) =>{
   if (req.cookies["user_id"]) {
     const templateVars = {"user": users[req.cookies["user_id"]]};
     res.render("urls_new", templateVars);
-    res.end();
+    return;
   }
-  
+
   res.redirect('/urls/login');
 });
 
+// route that generates new tiny URLS
 app.post("/urls", (req, res) => {
   const newStr = generateRandomString();
   let newUrlObj = {longURL: req.body.longURL, userID: req.cookies["user_id"]};
@@ -140,47 +132,61 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newStr}`);
 });
 
+// route shows the page to register new user
 app.get("/urls/register", (req, res) => {
   const templateVars = {"user": users[req.cookies["user_id"]]};
   res.render('urls_register', templateVars);
 });
 
+// route that registers new user
 app.post("/urls/register", (req, res) => {
-  let id = req.body.email;
+  let id = generateRandomString();
   let email = req.body.email;
   let password = req.body.password;
 
-  if (nonValidReg(email, password)) {
+  if (notValidReg(email, password)) {
     res.sendStatus(400);
-    res.end();
+    return;
   }
   users[id] = {id, email, password};
   res.cookie("user_id", id);
+
+  console.log(`the cookie should be ${id} and peek at users object`);
+  console.log(users);
+  
   res.redirect('/urls');
 });
 
+// route that shows the page to log in
 app.get("/urls/login", (req, res) => {
   const templateVars = {"user": users[req.cookies["user_id"]]};
   res.render("urls_login", templateVars);
 });
 
+// route that checks and processes the login and sets cookie
 app.post("/urls/login", (req, res) => {
-  let id = req.body.email;
+  console.log(` peek at users object`);
+  console.log(users);
+
+  let email = req.body.email;
   let pass = req.body.password;
-  if (isUser(id, pass)) {
-    res.cookie("user_id", id);
+  let isUser = getUser(email, pass);
+  console.log(isUser);
+  if (isUser) {
+    res.cookie("user_id", isUser);
     res.redirect('/urls');
-    res.end();
+    return;
   }
   res.sendStatus(403);
 });
 
+// route that redirects using the short url to the long url
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
-// route to edit URLS page
+// route to edit URLS page/ shows the details of one URL
 app.get("/urls/:shortURL", (req, res) => {
   let msg = '';
   if (!req.cookies["user_id"]) {
@@ -194,22 +200,10 @@ app.get("/urls/:shortURL", (req, res) => {
     msg: msg
   };
   res.render("urls_show", templateVars);
-  // let msg = '';
-  // //If not logged in display message to log in or register
-  // if (!req.cookies["user_id"]){
-  //   msg = "Please login"
-  //   console.log("Needs to login");
-  //   // res.send('Please login or Register');
-  //   // res.redirect('/urls/login');
-  // }
-  // let test = urlsForUser(req.cookies["user_id"]);
-  // const templateVars = {urls: test, "user": users[req.cookies["user_id"]], msg: msg};
-  // res.render("urls_index", templateVars);
 });
 
 
-
-// route to edit URLS
+// route to process the edit urls (only the specific user can edit)
 app.post("/urls/:shortURL", (req, res) => {
   //if the cookie matches the userID
   console.log(`Is it getting the short URL? ${req.params.shortURL}`);
@@ -220,7 +214,7 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
-// route to remoce URL entries
+// route to remove URL entries  (only the specific user can delete)
 app.post("/urls/:shortURL/delete", (req, res) => {
   //if the cookie matches the userID
   if (isOwnURL(req.cookies["user_id"], req.params.shortURL)) {
@@ -235,9 +229,6 @@ app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect('/urls');
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
